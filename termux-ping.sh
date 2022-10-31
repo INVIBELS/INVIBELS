41357690ddfbf06728ffb0c48512d35d89708c6e
INVIBELS <117050404+INVIBELS@users.noreply.github.com>
2022-10-31 09:31:36 +0800

    Create termux-ping.sh
    Used to check internet signal speed.

---
+++ termux-ping.sh
@@ -0,0 +1,186 @@
+#-- bahan pewarnaan --#
+clear
+blue='\033[34;1m'
+green='\033[32;1m'
+purple='\033[35;1m'
+cyan='\033[36;1m'
+red='\033[31;1m'
+white='\033[37;1m'
+yellow='\033[33;1m'
+
+
+#-- gambar boleh di modif klo ngerti --#
+sleep 1
+echo $purple'████████████████████████▀▀▀░░░░░░░▀▀▀█████████████████████████'
+sleep 0
+echo $purple"█████████████████████▀░░░░░░░░░░░░░░░░░▀██████████████████████"
+sleep 0
+echo $purple"████████████████████│░░░░░░░░░░░░░░░░░░░│█████████████████████"
+sleep 0
+echo $purple"███████████████████▌│░░░░░░░░░░░░░░░░░░░│▐████████████████████"
+sleep 0
+echo $purple"███████████████████░└┐░░░░░░░░░░░░░░░░░┌┘░████████████████████"
+sleep 0
+echo $purple"███████████████████░░└┐░░░░░░░░░░░░░░░┌┘░░████████████████████"
+sleep 0
+echo $purple"███████████████████░░┌┘▄▄▄▄▄░░░░░▄▄▄▄▄└┐░░████████████████████"
+sleep 0
+echo $purple"███████████████████▌░│██████▌░░░▐██████│░▐████████████████████"
+sleep 0
+echo $purple"████████████████████░│▐███▀▀░░▄░░▀▀███▌│░█████████████████████"
+sleep 0
+echo $purple"███████████████████▀─┘░░░░░░░▐█▌░░░░░░░└─▀████████████████████"
+sleep 0
+echo $purple"███████████████████▄░░░▄▄▄▓░░▀█▀░░▓▄▄▄░░░▄████████████████████"
+sleep 0
+echo $purple"██████████████████████░░▐█─┬┬┬┬┬┬┬─█▌░░███████████████████████"
+sleep 0
+echo $purple"█████████████████████▌░░░▀┬┼┼┼┼┼┼┼┬▀░░░▐██████████████████████"
+sleep 0
+echo $purple"██████████████████████▄░░░└┴┴┴┴┴┴┴┘░░░▄███████████████████████"
+sleep 0
+echo $purple"████████████████████████▄░░░░░░░░░░░▄█████████████████████████"
+sleep 0
+echo $purple"███████████████████████████▄▄▄▄▄▄▄████████████████████████████"
+sleep 0
+
+
+#-- tampilan awal --#
+echo $white"                         ╔══════════╗"
+echo $white"=========================║" $red"INVIBELS" $white"║========================="
+echo $white"                         ╚══════════╝"
+echo
+echo
+sleep 1
+echo $cyan"[1] " $red"Kecepatan jaringan"
+sleep 1
+echo $cyan"[0] " $red"Exit"
+echo
+sleep 1
+echo $white
+read -p "Pilih : " bro
+if [ $bro = 1 ] || [ $bro = 1 ]
+then
+clear
+
+
+#-- judul --#
+toilet -f big -F gay INTERNET
+sleep 1
+clear
+
+
+#-- sinyal internet --#
+echo $yellow'Process'
+sleep 1
+clear
+echo $yellow'Process.'
+sleep 1
+clear
+echo $yellow'Process..'
+sleep 1
+clear
+echo $yellow'Process...'
+sleep 2
+clear
+echo $yellow'complate!'
+sleep 0
+clear
+echo $white'jika ingin keluar tekan '$red'[CTRL+Z]'$green
+ping -s1000 1.1.1.1
+fi
+if [ $bro = 0 ] || [ $bro = 0 ]
+then
+
+
+#-- effect loading --#
+clear
+echo $blue'[ '$yellow'#'$red'-------------------'$blue' ]'$yellow'(5%)\r'
+sleep 1
+clear
+echo $blue'[ '$yellow'##'$red'------------------'$blue' ]'$yellow'(10%)\r'
+sleep 0
+clear
+echo $blue'[ '$yellow'###'$red'-----------------'$blue' ]'$yellow'(15%)\r'
+sleep 0
+clear
+echo $blue'[ '$yellow'####'$red'----------------'$blue' ]'$yellow'(20%)\r'
+sleep 0
+clear
+echo $blue'[ '$yellow'#####'$red'---------------'$blue' ]'$yellow'(25%)\r'
+sleep 1
+clear
+echo $blue'[ '$yellow'######'$red'--------------'$blue' ]'$yellow'(30%)\r'
+sleep 0
+clear
+echo $blue'[ '$yellow'#######'$red'-------------'$blue' ]'$yellow'(35%)\r'
+sleep 0
+clear
+echo $blue'[ '$yellow'########'$red'------------'$blue' ]'$yellow'(40%)\r'
+sleep 1
+clear
+echo $blue'[ '$yellow'#########'$red'-----------'$blue' ]'$yellow'(45%)\r'
+sleep 1
+clear
+echo $blue'[ '$yellow'##########'$red'----------'$blue']'$yellow'(50%)\r'
+sleep 0
+clear
+echo $blue'[ '$yellow'###########'$red'---------'$blue' ]'$yellow'(55%)\r'
+sleep 0
+clear
+echo $blue'[ '$yellow'############'$red'--------'$blue' ]'$yellow'(60%)\r'
+sleep 0
+clear
+echo $blue'[ '$yellow'#############'$red'-------'$blue' ]'$yellow'(65%)\r'
+sleep 0
+clear
+echo $blue'[ '$yellow'##############'$red'------'$blue' ]'$yellow'(70%)\r'
+sleep 0
+clear
+echo $blue'[ '$yellow'###############'$red'-----'$blue' ]'$yellow'(75%)\r'
+sleep 0
+clear
+echo $blue'[ '$yellow'################'$red'----'$blue' ]'$yellow'(80%)\r'
+sleep 1
+clear
+echo $blue'[ '$yellow'#################'$red'---'$blue' ]'$yellow'(85%)\r'
+sleep 1
+clear
+echo $blue'[ '$yellow'##################'$red'--'$blue' ]'$yellow'(90%)\r'
+sleep 0
+clear
+echo $blue'[ '$yellow'###################'$red'-'$blue' ]'$yellow'(95%)\r'
+sleep 1
+clear
+echo $blue'[ '$yellow'####################'$blue' ]'$yellow'(100%)\r'
+
+
+#-- exit --#
+clear
+sleep 2
+echo $white'Welcome to Termux!'
+echo
+echo $white'Docs:      https://termux.dev/docs'
+echo $white'Donate:    https://termux.dev/donate'
+echo $white'Community: https://termux.dev/community'
+echo
+echo $white'Working with packages:'
+echo
+echo $white'- Search: pkg search <query>'
+echo $white'- Install: pkg install <package>'
+echo $white'- Upgrade: pkg upgrade'
+echo
+echo $white'Subscribing to additional repositories:'
+echo
+echo $white'- Root: pkg install root-repo'
+echo $white'- X11: pkg install x11-repo'
+echo
+echo $white'For fixing any repository issues,'
+echo $white"try 'termux-change-repo' command."
+echo
+echo
+exit
+fi
+
+
+#-- trimaksih sudah install sc invibels --#
